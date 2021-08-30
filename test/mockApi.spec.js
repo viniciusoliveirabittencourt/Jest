e332ad56b9c3d43b287818cc864c38d9694b1f4b
viniciusoliveirabittencourt <viniciusbittencourt2003@gmail.com>
2021-08-30 00:20:51 -0300

    feito o lint de mockApi

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -1,4 +1,5 @@
 const api = require('../src/mockApi');
+
 jest.mock('../src/mockApi');
 
 /*
@@ -24,7 +25,7 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('2 - Verifica o usuário', () => {
-  const user = {
+  const tonicao = {
     gender: 'male',
     name: { title: 'Ms', first: 'Antônio', last: 'Britto' },
     location: {
@@ -36,8 +37,8 @@ describe('2 - Verifica o usuário', () => {
       coordinates: { latitude: '26.2451', longitude: '45.2995' },
       timezone: {
         offset: '+5:30',
-        description: 'Bombay, Calcutta, Madras, New Delhi'
-      }
+        description: 'Bombay, Calcutta, Madras, New Delhi',
+      },
     },
     email: 'tunico@bol.com.br',
     login: {
@@ -47,7 +48,7 @@ describe('2 - Verifica o usuário', () => {
       salt: 'XKOOGc2x',
       md5: '8cb7b4686f3869247b3ed189de780ea6',
       sha1: 'c24641f415cf36f4494ea4007fb3d77b47a6aad5',
-      sha256: 'a7bdd079ead0adf21f30cee5b94e5581a9fa0d5fc8b3c1881dbc864dabc55a80'
+      sha256: 'a7bdd079ead0adf21f30cee5b94e5581a9fa0d5fc8b3c1881dbc864dabc55a80',
     },
     dob: { date: '1965-10-01T06:35:49.694Z', age: 55 },
     registered: { date: '2009-02-11T05:48:39.772Z', age: 11 },
@@ -57,13 +58,12 @@ describe('2 - Verifica o usuário', () => {
     picture: {
       large: 'https://randomuser.me/api/portraits/women/7.jpg',
       medium: 'https://randomuser.me/api/portraits/med/women/7.jpg',
-      thumbnail: 'https://randomuser.me/api/portraits/thumb/women/7.jpg'
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/women/7.jpg',
     },
-    nat: 'IE'
-  }
-  
+    nat: 'IE',
+  };
 
-  api.fetchURL.mockResolvedValue(user);
+  api.fetchURL.mockResolvedValue(tonicao);
 
   test('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
